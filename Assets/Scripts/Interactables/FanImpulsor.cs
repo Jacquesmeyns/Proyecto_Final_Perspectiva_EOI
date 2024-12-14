b8bf9a7a9bf66922e980210113a8265647f5a519
Jacquesmeyns <jack_aguila55@hotmail.com>
2024-12-14 20:36:30 +0100

    - Fan script and prefab created.

---
+++ Assets/Scripts/Interactables/FanImpulsor.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class FanImpulsor : MonoBehaviour
+{
+    [SerializeField] float fanStrength = 1.5f;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(fanStrength * transform.up, ForceMode.Acceleration);
+        }
+    }
+}
