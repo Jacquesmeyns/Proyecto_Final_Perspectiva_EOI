a5fda7a5bbc54c3798e0b8e1301cf44bef91df1b
Jacquesmeyns <jack_aguila55@hotmail.com>
2024-12-18 13:39:43 +0100

    Checkpoint VFX tweaked.

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CheckpointController : MonoBehaviour
 {
     [SerializeField] public Mesh gizmoMesh;
     [SerializeField] GameManager gameManager;
     [SerializeField] private Transform spawnPosition;
+    [SerializeField] ParticleSystem idleParticles;
+    [SerializeField] ParticleSystem activationParticles;
+    [SerializeField] List<ParticleSystem> RayCenters;
+    [SerializeField] ParticleSystem FireCenter;
+    
+    // private List<ParticleSystem> activeParticles;
     public Vector3 SpawnPosition => spawnPosition.position;
     public Transform SpawnTransform => spawnPosition;
     //[SerializeField] private CheckpointController nextCheckpoint;
@@ -15,13 +23,40 @@ public class CheckpointController : MonoBehaviour
     {
         if(gameManager == null)
             throw new ArgumentNullException("gameManager");
+        // activeParticles = new List<ParticleSystem>();
+        //
+        // foreach (var particleSystem in idleParticles.gameObject.GetComponentsInChildren<ParticleSystem>())
+        // {
+        //     if (particleSystem.isPlaying)
+        //     {
+        //         activeParticles.Add(particleSystem);
+        //     }
+        // }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // var idleParticlesMain = idleParticles.main;
+            // idleParticlesMain.loop = false;
+            // idleParticles.Stop();
+            foreach (var ray in RayCenters)
+            {
+                ray.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+
+            FireCenter.Stop();
+            
             gameManager.currentCheckpoint = this;
+            activationParticles.gameObject.SetActive(true);
+            activationParticles.Play(true);
+            // foreach (var particleSystem in activationParticles.gameObject.GetComponentsInChildren<ParticleSystem>())
+            // {
+            //     particleSystem.Play();
+            // }
+            
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
     
