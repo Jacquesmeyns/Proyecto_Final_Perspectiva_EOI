89c066ae046f864afe2432c0f1f80067a802710d
Jacquesmeyns <jack_aguila55@hotmail.com>
2024-12-18 10:11:20 +0100

    Cleaning a bit of code

--- Assets/Scripts/Hazards/MoveSaw.cs
+++ Assets/Scripts/Hazards/MoveSaw.cs
@@ -15,28 +15,18 @@ public class MoveSaw : MonoBehaviour
     
     private IEnumerator moveLoop;
     private Rigidbody rb;
-    // private Vector3 moveDirection;
-    //private float rewind;
-    //private Vector3 direction;
-
 
     private void Awake()
     {
         timeToCompleteMove = Mathf.Abs(moveDistance / moveSpeed);
         timeToCompleteRotation = Mathf.Abs(1f/(rotationRPM / 60f)); //60 seg in 1 min ||| freq = 1 / T (rotation period)
-        // moveDirection = transform.right;
         Start = transform.position;
         End = Start + moveDistance * transform.right;
-        //direction = (End - Start).normalized;
         rb = GetComponent<Rigidbody>();
-
-        //rewind = Start.magnitude > End.magnitude ? 1 : -1;
         
         rb.DOMove(End, timeToCompleteMove).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
         rb.DORotate(new Vector3(0f, 0f, 360f), timeToCompleteRotation, RotateMode.LocalAxisAdd)
             .SetLoops(-1).SetEase(Ease.Linear);
-        // moveLoop = MoveCoroutine();
-        // StartCoroutine(moveLoop);
     }
 
     private void OnDrawGizmosSelected()
