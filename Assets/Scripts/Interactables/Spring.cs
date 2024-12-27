d5f7931e633f99ab919fa882b4ce750a82b11457
Jacquesmeyns <jack_aguila55@hotmail.com>
2024-12-28 00:10:51 +0100

    - Basic spring prefab created - Base for level 5 created

---
+++ Assets/Scripts/Interactables/Spring.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class Spring : MonoBehaviour
+    {
+        SpringJoint spring;
+        [SerializeField] private float minSpring = 10;
+        [SerializeField] private float maxSpring = 300;
+
+        private void Start()
+        {
+            spring = GetComponentInParent<SpringJoint>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                spring.spring = maxSpring;
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                spring.spring = minSpring;
+            }
+        }
+    }
+}
