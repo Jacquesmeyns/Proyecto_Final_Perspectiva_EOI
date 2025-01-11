1a7130d0668ee05322ca58cfd0a5de0b40789d89
Jacquesmeyns <jack_aguila55@hotmail.com>
2025-01-11 19:47:04 +0100

    Cleaner transition from Menu to Game.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,16 +1,43 @@
+using System;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Image whiteForeground;
+    private IEnumerator _startGameCoroutine;
+    private float startDelay = 2f;  //seconds
+
+    private void Awake()
+    {
+        _startGameCoroutine = StartGameCoroutine();
+    }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level1");
+        StartCoroutine(_startGameCoroutine);
     }
 
+    IEnumerator StartGameCoroutine()
+    {
+        DOVirtual.Float(0f, 1f, startDelay, (_value) =>
+        {
+            whiteForeground.color = new Color(1, 1, 1, _value);
+        });
+        yield return new WaitForSeconds(startDelay+0.1f);
+        SceneManager.LoadScene("Level1");
+    }
+    
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void OnDestroy()
+    {
+        _startGameCoroutine = null;
     }
 }
