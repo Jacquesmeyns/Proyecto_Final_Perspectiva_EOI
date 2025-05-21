2e30397c0b5796dcb10d53249e00fc6d159b7c49
Jacquesmeyns <jack_aguila55@hotmail.com>
2025-05-21 16:26:03 +0200

    - Level 5 improvement - Spring changes

--- Assets/Scripts/Interactables/Spring.cs
+++ Assets/Scripts/Interactables/Spring.cs
@@ -5,13 +5,25 @@ namespace Interactables
 {
     public class Spring : MonoBehaviour
     {
-        SpringJoint spring;
+        [SerializeField] SpringJoint spring;
         [SerializeField] private float minSpring = 10;
         [SerializeField] private float maxSpring = 300;
+        private Vector3 platformPos, anchorPos;
+        [SerializeField] private LineRenderer line;
 
-        private void Start()
+        private void Update()
         {
-            spring = GetComponentInParent<SpringJoint>();
+            platformPos = spring.connectedBody.transform.position + spring.anchor*transform.parent.up.normalized.y;
+            anchorPos = spring.transform.position;//StartPos + moveDistance * transform.right;
+            var StartEnd = anchorPos - platformPos;
+
+            line.SetPositions(new Vector3[]{platformPos, platformPos + (StartEnd*0.01f), platformPos + (StartEnd*0.99f), anchorPos});
+        }
+        
+        private void FixedUpdate()
+        {
+            spring.connectedBody.AddForce(-transform.parent.up.normalized * spring.connectedBody.mass);
+            //??
         }
 
         private void OnCollisionEnter(Collision other)
@@ -28,6 +40,14 @@ namespace Interactables
             {
                 spring.spring = minSpring;
             }
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            //Draw expectedEnd
+            platformPos = spring.connectedBody.transform.position + spring.anchor*transform.parent.up.normalized.y;
+            anchorPos = spring.transform.position;//StartPos + moveDistance * transform.right;
+            Gizmos.DrawLine( platformPos, anchorPos);
         }
     }
 }
